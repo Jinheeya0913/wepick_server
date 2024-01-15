001f883834da00de0496e4f69da8058c9ca5f4e3
Jinheeya0913 <taran0913@naver,com>
2024-01-15 15:59:30 +0900

    [23.01.15] 리뷰 사진 조회

---
+++ src/main/java/com/twojin/wooritheseday/file/dto/ReviewImgEntity.java
@@ -0,0 +1,38 @@
+package com.twojin.wooritheseday.file.dto;
+
+import com.twojin.wooritheseday.common.utils.BooleanToYNConverterUtil;
+import com.twojin.wooritheseday.common.vo.FileEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "woori_review_img")
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewImgEntity extends FileEntity {
+
+    @Column(nullable = false)
+    @NotBlank
+    protected String userId;
+
+    @Convert(converter = BooleanToYNConverterUtil.class)
+    protected boolean useAt;
+
+
+    @Builder
+    public ReviewImgEntity(String userId, String filePath, String fileName, String fileExtension, boolean useAt) {
+        this.userId = userId;
+        super.filePath = filePath;
+        super.fileName = fileName;
+        this.fileExtension = fileExtension;
+        this.useAt = useAt;
+
+    }
+
+
+}
